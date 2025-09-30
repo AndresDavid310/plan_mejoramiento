b3a576e6e7c920c75500f172425ecac889b4894b
Andres David Valencia Rojas <anderdavidrojasvalencia@gmail.com>
2025-09-29 22:08:06 -0600

    Ejercicios

--- Actividades.py
+++ Actividades.py
@@ -10,20 +10,34 @@ print(f"el doble del numero {numero1} es {numero1*2}.")
 
 #3 programa basico
 
-# Listas, tuplas, diccionarios y cadenas
 frutas = ["manzana", "pera", "uva"]
 precios = (2000, 1500, 1800)  
 inventario = {"manzana": 10, "pera": 8, "uva": 15}
 
 usuario = "admin"
 
-# Bucle while con condicionales
 intentos = 0
 while intentos < 3:
     entrada = input("Escribe el usuario: ")
     if entrada == usuario:
-        print(f"Bienvenido {entrada}. Tenemos {inventario} en stock.")
+        print(f"Bienvenido {entrada}. Tenemos {inventario} en la seccion de fruta.")
         break
     else:
-        print("Usuario incorrecto, intenta de nuevo.")
+        print("Usuario incorrecto, intenta de nuevo mas tarde.")
         intentos += 1
+        
+        
+        
+        
+#ejemplo sencillo
+
+class Persona:
+    def __init__(self, nombre, edad):   # Constructor con atributos
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):   # Método
+        print(f"Hola, me llamo {self.nombre} y tengo {self.edad} años.")
+
+p1 = Persona("Lucía", 21)
+p1.saludar()
